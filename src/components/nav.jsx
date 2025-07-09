b52a0c1e0338b89596a4e3ca44a23008a54e289e
Vented5 <venteddiagram@gmail.com>
2025-07-08 18:22:52 -0600

    04: Navbar fixed, svg added

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,16 +2,33 @@ import { Link } from 'react-router-dom'
 
 export default function Nav() {
     return <>
-       <nav id='nav'>
-            <Link to='/somos/'>
-                <img id='logo' src='./SOMOSac_final_blanco.png' alt='logo' />
-            </Link>
-            <div id='links'>
-                <Link to='/somos/eventos'>Eventos</Link>
-                <Link to='/somos/projectos'>Proyectos</Link>
-                <Link to='/somos/programas'>Programas</Link>
-                <Link to='/somos/transparencia'>Transparencia</Link>
-            </div>
-        </nav>
+        <nav id='nav'>
+    <svg viewBox="0 0 1440 130" preserveAspectRatio="none"
+         style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: -1 }}>
+        <path d="M0 0V126C412 7 1440 145 1440 0H0Z"
+              fill="url(#paint0_linear_17_3)" stroke="black"/>
+        <defs>
+            <linearGradient id="paint0_linear_17_3" x1="0" y1="41" x2="1440" y2="41" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#363636"/>
+                <stop offset="0.158654" stop-color="#8A3677"/>
+                <stop offset="0.774038" stop-color="#00ACA6"/>
+                <stop offset="1" stop-color="#CBCBCB"/>
+            </linearGradient>
+        </defs>
+    </svg>
+
+    <Link to='/somos/'>
+        <img id='logo' src='./SOMOSac_final_blanco.png' alt='logo' />
+    </Link>
+
+    <div id='links'>
+        <Link to='/somos/eventos'>Eventos</Link>
+        <Link to='/somos/projectos'>Proyectos</Link>
+        <Link to='/somos/programas'>Programas</Link>
+        <Link to='/somos/transparencia'>Transparencia</Link>
+    </div>
+</nav>
+
+
     </>
 }
