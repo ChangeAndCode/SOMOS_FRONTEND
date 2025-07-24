64aff1e18cfeaf87deda9b2024f6f41056939c18
Jaziel Parra <52476295+Vented5@users.noreply.github.com>
2025-07-24 21:13:48 +0000

    015: Form layout added.

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,71 @@
+import './style.css'
+import React, { useEffect, useRef, useState } from 'react';
+
+export default function Form({ fields, onSubmit, submitText = 'Enviar' }) {
+    
+    // fields: array de objetos { name, label, type, placeholder, required }
+    const initialFormState = fields.reduce((acc, field) => {
+        acc[field.name] = '';
+        return acc;
+    }, {});
+
+    const [formData, setFormData] = useState(initialFormState);
+    const [showForm, setShowForm] = useState(true);
+
+    const handleChange = (e) => {
+        setFormData({
+        ...formData,
+        [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(formData);
+        // Opcional: limpiar formulario después de enviar
+        setFormData(initialFormState);
+    };
+    
+    const formRef = useRef(null)
+
+    useEffect(() => {
+        const handleOutsideClick = (e) => {
+            if(formRef.current && !formRef.current.contains(e.target)) {
+                setShowForm(false)
+            }
+        }
+        document.addEventListener('mousedown', handleOutsideClick)
+        return () => {
+            document.removeEventListener('mousedown', handleOutsideClick)
+        }
+    }, [showForm])
+
+    return <>
+        <button className='formDropdown' onClick={() => setShowForm(prev => !prev)}>
+            <img src="public/icons/plus.svg" alt="plus icon" />
+            {showForm ? 'Ocultar formulario' : 'Mostrar formulario'}</button>
+
+        {showForm && (
+        <form onSubmit={handleSubmit} ref={formRef} className='form'>
+            {fields.map(({ name, label, type = 'text', placeholder = '', required = false }) => (
+                <div key={name}>
+                <label htmlFor={name} style={{ display: 'block', marginBottom: '.3rem' }}>
+                    {label}
+                </label>
+                <input
+                    id={name}
+                    name={name}
+                    type={type}
+                    placeholder={placeholder}
+                    value={formData[name]}
+                    onChange={handleChange}
+                    required={required}
+                    style={{ padding: '.5rem', width: '100%' }}
+                />
+                </div>
+            ))}
+            <button type="submit">{submitText}</button>
+        </form>
+        )}
+    </>
+}
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -4,13 +4,22 @@ import Footer from '../../components/Footer/Footer'
 import CardCarrousel from "../../components/CardCarrousel/CardCarrousel"
 import { data } from './projects.json'
 
+import Form from '../../components/Form/Form'
+
 export default function Projects() {
     
+    const addProjectForm = [
+        {name: "projectName", label: "Nombre del proyecto", type: "text", placeholder: "Ingrese nombre del proyecto", required: true},
+        {name: "description", label: "Descripcion", type: "text", placeholder: "Insgrese descripcion del proyecto", required: true}
+    ]
+
     return <>
        <Nav/>
        <article className="first">
             <h2>Bienvenido a la seccion de proyectos</h2>
             <CardCarrousel array={data} imgRoute={"./images/projects/"}></CardCarrousel>
+            
+            <Form fields={addProjectForm} onSubmit={(data) => console.log(data)} submitText='Enviar'></Form>
         </article>
         <Footer/> 
     </>
