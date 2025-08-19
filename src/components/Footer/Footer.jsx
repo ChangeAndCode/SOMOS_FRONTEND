e78ad38b6a7cc9b15c00c2218be0d706a81a559c
Gibrán Pérez <gperezfavela@gmail.com>
2025-08-19 12:34:49 -0600

    update footer component
    added dynamic values
    deleted useless code
    comment style.css

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,38 +1,58 @@
-import './style.css'
+import "./style.css";
+import FooterData from "../../../data/footer.json";
 
 export default function Footer() {
-    return <>
-        <footer>
-            <div>
-                <h2>Somos Unión contacto</h2>
-                <p>
-                    Lomas del Coronel 4600, 
-                    Lomas de Santa Rosa, <br/> 
-                    Lomas Universidad, Juventud Nte, <br />
-                    31124 Chihuahua, Chih. <br />
-
+  return (
+    <>
+      <footer className="bg-[var(--bright)] text-[var(--light)] px-5 pb-10">
+        <h2 className="text-center xs:text-lg md:text-2xl lg:text-4xl font-extrabold xs:my-5 md:my-10">
+          {FooterData.contact}
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 text-start">
+          <div className="flex flex-col items-start gap-2  py-2">
+            {FooterData.location.map((item, index) => {
+              return (
+                <p key={index} className="font-bold">
+                  {item}
                 </p>
-            </div>
-            <div>
-                <a href='https://www.facebook.com/profile.php?id=61557303635377'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M128,24A104,104,0,1,0,232,128,104.11,104.11,0,0,0,128,24Zm8,191.63V152h24a8,8,0,0,0,0-16H136V112a16,16,0,0,1,16-16h16a8,8,0,0,0,0-16H152a32,32,0,0,0-32,32v24H96a8,8,0,0,0,0,16h24v63.63a88,88,0,1,1,16,0Z"></path></svg>
-                    <p>Somos Unión</p>
+              );
+            })}
+          </div>
+          <div className="grid lg:grid-cols-2 font-bold gap-2  py-2">
+            {FooterData.socialMedia.map((item, index) => {
+              const SvgIcon = (
+                <svg
+                  xmlns={item.xmlns}
+                  width="50"
+                  height="50"
+                  fill="currentColor"
+                  viewBox="0 0 256 256"
+                >
+                  <path d={item.path}></path>
+                </svg>
+              );
+              return item.href ? (
+                <a
+                  key={index}
+                  href={item.href}
+                  className="flex items-center justify-start gap-2"
+                >
+                  {SvgIcon}
+                  <p>{item.value}</p>
                 </a>
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M187.58,144.84l-32-16a8,8,0,0,0-8,.5l-14.69,9.8a40.55,40.55,0,0,1-16-16l9.8-14.69a8,8,0,0,0,.5-8l-16-32A8,8,0,0,0,104,64a40,40,0,0,0-40,40,88.1,88.1,0,0,0,88,88,40,40,0,0,0,40-40A8,8,0,0,0,187.58,144.84ZM152,176a72.08,72.08,0,0,1-72-72A24,24,0,0,1,99.29,80.46l11.48,23L101,118a8,8,0,0,0-.73,7.51,56.47,56.47,0,0,0,30.15,30.15A8,8,0,0,0,138,155l14.61-9.74,23,11.48A24,24,0,0,1,152,176ZM128,24A104,104,0,0,0,36.18,176.88L24.83,210.93a16,16,0,0,0,20.24,20.24l34.05-11.35A104,104,0,1,0,128,24Zm0,192a87.87,87.87,0,0,1-44.06-11.81,8,8,0,0,0-6.54-.67L40,216,52.47,178.6a8,8,0,0,0-.66-6.54A88,88,0,1,1,128,216Z"></path></svg>
-                    <p>614 415 6936</p>
+              ) : (
+                <span
+                  key={index}
+                  className="flex items-center justify-start gap-2"
+                >
+                  {SvgIcon}
+                  <p>{item.value}</p>
                 </span>
-            </div>
-            <div>
-                <a href='https://www.instagram.com/somosunionac?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M128,80a48,48,0,1,0,48,48A48.05,48.05,0,0,0,128,80Zm0,80a32,32,0,1,1,32-32A32,32,0,0,1,128,160ZM176,24H80A56.06,56.06,0,0,0,24,80v96a56.06,56.06,0,0,0,56,56h96a56.06,56.06,0,0,0,56-56V80A56.06,56.06,0,0,0,176,24Zm40,152a40,40,0,0,1-40,40H80a40,40,0,0,1-40-40V80A40,40,0,0,1,80,40h96a40,40,0,0,1,40,40ZM192,76a12,12,0,1,1-12-12A12,12,0,0,1,192,76Z"></path></svg>
-                    <p>somosunionac</p>
-                </a>
-                <span>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M224,48H32a8,8,0,0,0-8,8V192a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A8,8,0,0,0,224,48Zm-96,85.15L52.57,64H203.43ZM98.71,128,40,181.81V74.19Zm11.84,10.85,12,11.05a8,8,0,0,0,10.82,0l12-11.05,58,53.15H52.57ZM157.29,128,216,74.18V181.82Z"></path></svg>
-                    <p>esperechihuahua@gmail.com </p>
-                </span>
-            </div>
-        </footer>
+              );
+            })}
+          </div>
+        </div>
+      </footer>
     </>
+  );
 }
